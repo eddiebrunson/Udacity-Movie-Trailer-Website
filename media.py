057bb5930bf1f8e94d73c5c1416cd19f0d646a82
Edward Brunson <eddiebrunson@gmail.com>
2017-01-03 23:32:50 -0500

    Updated per Pep8 Style Guide

--- media.py
+++ media.py
@@ -1,13 +1,14 @@
 import webbrowser
-#Movie class
+# Movie class
+
+
 class Movie():
-    #def__init is a constructor
+    # def__init is a constructor
+
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube, movie_rating):
-        #These are the instances variables 
+        # These are the instances variables
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
         self.rating = movie_rating
-
-    
